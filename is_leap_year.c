ec9b94781a8d51c7cd4b995112ce898fcbf8d504
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2021-06-11 00:27:36 -0400

    Determine if a year is a leap year example

---
+++ is_leap_year.c
@@ -0,0 +1,37 @@
+/*******************************************************************************
+*
+* Program: Leap year checker
+* 
+* Description: An example of how to determine whether a year is a leap year.
+*
+* Author: Kevin Browne @ https://portfoliocourses.com
+*
+*******************************************************************************/
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_leap_year(int year);
+
+int main(void)
+{
+  // determine which of the years from 1900-2100 are leap years
+  for (int y = 1900; y <= 2100; y++)
+  {
+    if (is_leap_year(y))
+      printf("%d LEAP YEAR\n", y);
+    else 
+      printf("%d\n", y);
+  }
+
+  return 0;
+}
+
+// Determines if a year is a leap year using this algorithm...
+//   en.wikipedia.org/w/index.php?title=Leap_year&oldid=1027751480#Algorithm
+bool is_leap_year(int year)
+{
+  if (year % 4 != 0) return false;
+  else if (year % 100 != 0) return true;
+  else if (year % 400 != 0) return false;
+  else return true;
+}
