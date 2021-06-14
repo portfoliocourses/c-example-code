abf8e94bf0b243b05752e715f6e197e9765735c1
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2021-06-14 11:06:46 -0400

    Dice roll simulator example

--- dice_roll.c
+++ dice_roll.c
@@ -15,13 +15,17 @@
 
 int main(void)
 {
+  // Seeds the random number generator
   srand( time(NULL) );
 
+  // dice stores the number of dice rolls, roll will store each roll
   int dice = 10;
   int roll = 0;
 
+  // conduct each dice roll and output the result
   for (int i = 1; i <= dice; i++)
   {
+    // rand() % 6 produces a random number between 0-5, add 1 to shift to 1-6
     roll = rand() % 6 + 1;
     printf("Dice %d: %d\n", i, roll);
   }
