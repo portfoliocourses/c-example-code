934648a05b4404c0dfc2901b55c35fc1b57e6f93
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2021-07-23 08:32:20 -0400

    strrchr function example

---
+++ strrchr.c
@@ -0,0 +1,31 @@
+/*******************************************************************************
+*
+* Program: strrchr demo
+* 
+* Description: A demonstration of how to use the strrchr function in C.
+*
+* YouTube Lesson: https://www.youtube.com/watch?v=R0qIYWo8igs 
+*
+* Author: Kevin Browne @ https://portfoliocourses.com
+*
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+  char s[] = "some,data,in,commas";
+  
+  // the function will return a pointer to the last char that matches ',' in s
+  char *comma = strrchr(s, ',');
+
+  // this effectively gives us a substring of s from the last matching char 
+  // to the end of the string
+  printf("%s\n", comma);
+  
+  // if no matching char is found, the function will return NULL
+  char *fail = strrchr(s, '@');
+  if (fail == NULL) printf("Could not find @!\n");
+
+  return 0;
+}
