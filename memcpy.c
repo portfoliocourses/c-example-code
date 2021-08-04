700ac4b3ba2a304b413d16b889f01e44fa2b301d
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2021-08-04 10:20:19 -0400

    memcpy function example

---
+++ memcpy.c
@@ -0,0 +1,38 @@
+/*******************************************************************************
+*
+* Program: memcpy demo
+* 
+* Description: Example of using the memcpy function in C.  The memcpy function 
+* allows us to copy a block of data located at an address in memory to another 
+* address in memory.
+*
+* YouTube Lesson: https://www.youtube.com/watch?v=R0qIYWo8igs 
+*
+* Author: Kevin Browne @ https://portfoliocourses.com
+*
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+  char s[] = "abcdefghijklmnopqrstuvwxyz";
+  char d[27];
+ 
+  // copy 27 chars worth of data from s to do... the 26 letters + the null 
+  // terminator
+  memcpy(d, s, sizeof(char) * 27);
+  printf("d: %s\n", d);
+
+  // copy 9 chars worth of data from the memory address of s shifted over by 
+  // 8 ints worth of memory addresses (using pointer arithmetic with s+8)
+  char e[10];
+  memcpy(e, s+8, sizeof(char) * 9);
+
+  // append the null terminator manually to the end of e since this time we 
+  // did not copy it from s
+  e[9] = '\0';
+  printf("e: %s\n", e);
+
+  return 0;
+}
