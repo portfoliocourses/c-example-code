7354502254b15c78493ddb677fee5d496be14862
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2021-08-06 08:58:13 -0400

    exit function example

---
+++ exit.c
@@ -0,0 +1,43 @@
+/*******************************************************************************
+*
+* Program: Exit function
+* 
+* Description: Examples of using the exit() function in C.
+*
+* YouTube Lesson: https://www.youtube.com/watch?v=R0qIYWo8igs 
+*
+* Author: Kevin Browne @ https://portfoliocourses.com
+*
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void func();
+
+int main(void)
+{
+  printf("Hello, World!\n");
+
+  // We can use exit to exit the program at any point during its execution
+  // the exit status of our program (0 for success, anything else is failure) 
+  // is given as an argument to the exit function.
+  // exit(1);
+  printf("Some other text!\n");
+
+  func();
+
+  // Normally our program exits when a return statement is encountered in main
+  return 0;
+}
+
+void func()
+{
+  printf("In the func()\n");
+
+  // With exit() we can exit the program in the middle of a function, e.g. 
+  // perhaps due to some error our program can on longer continue.  We can 
+  // use constants EXIT_FAILURE an EXIT_SUCCESS to exit with success or 
+  // failure.
+  exit(EXIT_FAILURE);
+  printf("Below the exit in func!\n");
+}
