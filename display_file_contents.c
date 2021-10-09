c2d43defd6e6a6acbc53875d3b44c2574b0d7ed1
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2021-10-08 23:46:31 -0400

    read and display the contents of a file example

--- display_file_contents.c
+++ display_file_contents.c
@@ -4,7 +4,7 @@
 * 
 * Description: Example of reading and printing a file's contents in C.
 *
-* YouTube Lesson: https://www.youtube.com/watch?v=R0qIYWo8igs 
+* YouTube Lesson: https://www.youtube.com/watch?v=fLPqn026DaE 
 *
 * Author: Kevin Browne @ https://portfoliocourses.com
 *
