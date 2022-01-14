3a2d83e0ef0848c703b49da546403be79fb29263
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-01-14 16:55:11 -0500

    defines global variable number

--- extern_share_global_variable/library.c
+++ extern_share_global_variable/library.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+// defines the global number variable, which actually allocates space in memory
+// for the variable, in contrast to only declaring it when we use extern 
 int number = 0;
+
+// define some simple functions for working with the global number variable
 
 void add_one()
 {
