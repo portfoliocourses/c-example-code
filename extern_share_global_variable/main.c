b824a7b644dcde9a402cecf3e1e861393d0a9fed
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-01-14 16:49:32 -0500

    uses global variable number by including library.h

---
+++ extern_share_global_variable/main.c
@@ -0,0 +1,19 @@
+//  ** Link to the code in the video description **
+//
+//  ** Subscribe for regular tutorials and courses **
+
+#include <stdio.h>
+#include "library.h"
+
+int main()
+{
+  print();
+  add_one();
+  add_one();
+  print();
+
+  number += 10;
+  print();
+
+  return 0;
+}
