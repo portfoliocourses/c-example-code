82a6fab535fe2777ac42a8c917f2c951cec6bbdd
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-01-14 16:52:12 -0500

    uses global variable number by including library.h

--- extern_share_global_variable/main.c
+++ extern_share_global_variable/main.c
@@ -1,17 +1,30 @@
-//  ** Link to the code in the video description **
-//
-//  ** Subscribe for regular tutorials and courses **
+/*******************************************************************************
+*
+* Program: Share Gobal Variable
+* 
+* Description: Example of sharing a global variable across multiple files using
+* the extern keyword.
+*
+* YouTube Lesson: https://www.youtube.com/watch?v=Wqm1tSzmqkY 
+*
+* Author: Kevin Browne @ https://portfoliocourses.com
+*
+*******************************************************************************/
 
 #include <stdio.h>
-#include "library.h"
+#include "library.h" 
 
 int main()
 {
+  // we can use the functions defined in our library as normal, and they will 
+  // have access to the global variable number
   print();
   add_one();
   add_one();
   print();
 
+  // but main.c ALSO has access to the global variable number because we 
+  // used extern to declare it in library.h which is included by main.c
   number += 10;
   print();
 
