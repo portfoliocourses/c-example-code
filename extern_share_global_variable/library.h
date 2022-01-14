089b7ba9c1e58136b802baf777db72a9414a11db
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-01-14 16:53:57 -0500

    declares global variable number by using extern

--- extern_share_global_variable/library.h
+++ extern_share_global_variable/library.h
@@ -1,3 +1,6 @@
+// extern declares the global variable number (but does not define it, i.e. no 
+// new space is allocated in memory, instead it uses the exsting global number 
+// variable)
 extern int number;
 
 void add_one();
