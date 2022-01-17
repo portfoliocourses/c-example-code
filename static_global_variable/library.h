f1bfaa117ad4edcc6cdb9f5e0645d07fa243f48e
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-01-17 01:41:00 -0500

    library header file

---
+++ static_global_variable/library.h
@@ -0,0 +1,7 @@
+// if number weren't defined using the static keyword, we might make it 
+// available to other files by using the extern keyword as done below...
+
+// extern int number;
+
+void add_one();
+void print();
