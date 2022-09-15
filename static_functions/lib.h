40455d2bb6029de1bf1cf0b5c8403145f5aaa048
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-09-15 13:17:21 -0400

    library header that defines library interface

---
+++ static_functions/lib.h
@@ -0,0 +1,5 @@
+// report_value() is an "interface" for this library that we WANT to expose to 
+// the rest of the program to use... ideally an interface like this is more 
+// stable in that it is less likely to change.
+
+void report_value(double value);
