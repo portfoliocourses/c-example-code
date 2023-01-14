8ab420cff875c953f0751d2aebb64b4b1d5fed18
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2023-01-14 16:18:44 -0500

    find the minium number in an array using recursion

---
+++ find_min_recursive.c
@@ -0,0 +1,36 @@
+/*******************************************************************************
+*
+* Program: Find Minimum Number In Array With Recursion
+* 
+* Description: Example of using recursion to find the smallest number in an 
+* array using C.
+*
+* YouTube Lesson: https://www.youtube.com/watch?v=iq9fiqIiLAc 
+*
+* Author: Kevin Browne @ https://portfoliocourses.com
+*
+*******************************************************************************/
+
+#include <stdio.h>
+
+int min(int array[], int n);
+
+int main()
+{
+  int array[] = {4,1,8,6,0,9,5,2,3,7};
+  int length = 10;
+  
+  printf("min: %d\n", min(array, length));
+  
+  return 0;
+}
+
+int min(int array[], int n)
+{
+  if (n == 1) return array[n - 1];
+  
+  int possible_min = min(array, n - 1);
+  
+  if (possible_min < array[n - 1]) return possible_min;
+  else return array[n - 1];
+}
